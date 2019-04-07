2605fd65d5699d8d97bf996fc550ed82a0807739
Malacrida <sara.malacrida@mail.polimi.it>
2019-04-07 19:07:00 +0200

    Change directory deliveries

--- src/main/java/it/polimi/isw2019/Model/Square.java
+++ src/main/java/it/polimi/isw2019/Model/Square.java
@@ -1,5 +1,7 @@
 package it.polimi.isw2019.Model;
 
+import java.util.ArrayList;
+
 public abstract class Square {
     private boolean sideN;
     private boolean sideE;
@@ -9,6 +11,24 @@ public abstract class Square {
     private int squareE;
     private int squareS;
     private int squareO;
+    private int squareID;
     private boolean spownpoint;
+    private ArrayList<Player> player;
 
+    Square (int squareID, int squareN, int squareE, int squareO, int squareS, boolean sideN,
+            boolean sideE, boolean sideO, boolean sideS){
+        this.squareID= squareID;
+        this.sideN=sideN;
+        this.sideE=sideE;
+        this.sideS=sideS;
+        this.sideO=sideO;
+        this.squareN=squareN;
+        this.squareE=squareE;
+        this.squareO=squareO;
+        this.squareS=squareS;
+    }
+
+    public ArrayList<Player> getPlayer() {
+        return player;
+    }
 }
