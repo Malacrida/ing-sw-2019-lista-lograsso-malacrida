36cc6858f35948cc26085c2820338f1e3c233f08
Malacrida <sara.malacrida@mail.polimi.it>
2019-04-21 18:58:45 +0200

    Change Player, PlayerBoard and Model

--- src/main/java/it/polimi/isw2019/Model/Player.java
+++ src/main/java/it/polimi/isw2019/Model/Player.java
@@ -1,40 +1,54 @@
 package it.polimi.isw2019.Model;
 
+import it.polimi.isw2019.Model.WeaponCard.WeaponCard;
+
+import java.util.ArrayList;
+
 public class Player {
    private String name;
    private ColorPlayer color;
-   private boolean selected;
-   private int position;
-   private boolean useFigure;
+   private Position position;
+   private ArrayList<WeaponCard> weaponCards = new ArrayList<>();
+   private PlayerBoard playerBoard;
 
-   public Player(String name, ColorPlayer color, boolean selected, int position, boolean useFigure) {
-      this.name = name;
-      this.color = color;
-      this.selected = selected;
-      this.position = position;
-      this.useFigure = useFigure;
-   }
 
-   public Player getPlayer() {
+    public Player(String name, ColorPlayer color, PlayerBoard playerBoard) {
+        this.name = name;
+        this.color = color;
+        position= new Position(-1, -1,null); // valori fuori dal range accettabile della mappa
+        this.playerBoard = playerBoard;
+    }
+
+
+    public Player getPlayer() {
       return this;
    }
 
-   public String getName() {
+    public String getName() {
       return this.name;
    }
 
-   public ColorPlayer getColor(){
+    public ColorPlayer getColor(){
       return this.color;
    }
 
-   public boolean getSelected() {
-      return this.selected;
-   }
+    public Position getPosition() {
+        return position;
+    }
 
-   public boolean getUseFigure() {
-      return this.useFigure;
-   }
-   public int getPosition() {
-      return this.position;
-   }
+    //nel model un metodo che unisce questo del player e quello con la gameboard
+    //Ricordarsi il cambio di stato
+    public void takeWeaponCards(WeaponCard insertWeaponCard, WeaponCard removeWeaponCard) {
+        if (weaponCards.size()<3) {
+            weaponCards.add(insertWeaponCard);
+        }
+        else{
+            weaponCards.remove(removeWeaponCard);
+            weaponCards.add(insertWeaponCard);
+        }
+    }
+
+
+
+
 }
