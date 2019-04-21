9ceedb169b80859725b0a23e4df8c082852ba6ce
alba <10503070@polimi.it>
2019-04-21 19:01:20 +0200

    Modification new classes

---
+++ src/main/java/it/polimi/isw2019/Controller/Reload.java
@@ -0,0 +1,11 @@
+package it.polimi.isw2019.Controller;
+
+import it.polimi.isw2019.Model.Events.PlayerMove;
+import it.polimi.isw2019.Model.Model;
+
+public class Reload implements VisitorAction {
+    @Override
+    public void visitModel(Model model, PlayerMove playerMove){
+
+    }
+}
---
+++ src/main/java/it/polimi/isw2019/Controller/RunReloadUseWeaponCard.java
@@ -0,0 +1,11 @@
+package it.polimi.isw2019.Controller;
+
+import it.polimi.isw2019.Model.Events.PlayerMove;
+import it.polimi.isw2019.Model.Model;
+
+public class RunReloadUseWeaponCard implements VisitorAction {
+    @Override
+    public void visitModel(Model model, PlayerMove playerMove){
+
+    }
+}
