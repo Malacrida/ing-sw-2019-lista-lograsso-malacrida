14c5092f48fb805fe75212e4b19ad8ac8381116d
Malacrida <sara.malacrida@mail.polimi.it>
2019-05-28 16:39:32 +0200

    fix method

--- src/main/java/it/polimi/isw2019/network/ServerRmi.java
+++ src/main/java/it/polimi/isw2019/network/ServerRmi.java
@@ -7,8 +7,13 @@ import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ServerRmi {
+
+    public ServerRmi ()throws RemoteException {
+        super();
+    }
 
     //aprire connessione
 
