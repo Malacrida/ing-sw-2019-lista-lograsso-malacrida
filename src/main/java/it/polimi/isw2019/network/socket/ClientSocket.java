8722330a602679c53a14cf7251b5184f159dff26
Davide Lista <davide.lista@mail.polimi.it>
2019-07-03 13:08:25 +0200

    fixed Socket

--- src/main/java/it/polimi/isw2019/network/socket/ClientSocket.java
+++ src/main/java/it/polimi/isw2019/network/socket/ClientSocket.java
@@ -32,11 +32,6 @@ public class ClientSocket extends Thread implements ClientInterface {
     private VirtualViewSocket virtualViewSocket;
 
     @Override
-    public void createReload(String nicknamePlayer) throws RemoteException {
-
-    }
-
-    @Override
     public void createUpdateView(String nicknamePlayer, String gameBoardDescription, int[][] playersDescription, int[][] featuresOfPlayersAvailable, String[][] weaponCardDescription, String[][] powerUpCardDescription, boolean notifyAll) throws RemoteException {
 
     }
@@ -119,13 +114,8 @@ public class ClientSocket extends Thread implements ClientInterface {
     }
 
     @Override
-    public void createActionMessage(String nickname) throws RemoteException {
-
-    }
-
-    @Override
-    public void createSetupView(String idMoveMessage, ArrayList<String> colorAvailable) throws RemoteException {
-
+    public void createActionMessage(String nicknamePlayer, String[] actionYouCanPerform, ArrayList<String> actionPlayerCanPerform, ArrayList<Integer> intIdAction) throws RemoteException {
+        
     }
 
     @Override
@@ -134,14 +124,13 @@ public class ClientSocket extends Thread implements ClientInterface {
     }
 
     @Override
-    public void createGrab(String nicknamePlayer) throws RemoteException {
+    public void createGrab(String nicknamePlayer, String error, String[] weaponCardAvailable, int[] featuresAvailable, boolean grabWeapon, String ammoTileDescription, String powerUpDescription) throws RemoteException {
 
     }
 
-
     @Override
-    public void createOkRegistration(String nicknamePlayer, String actionHero, ArrayList<String> colors) throws RemoteException {
-        EndRegistration endRegistration = new EndRegistration(nicknamePlayer);
+    public void createReload(String nicknamePlayer, int[] featuresAvailable, int[] weaponYouCanReload, String error) throws RemoteException {
+
     }
 
     @Override
@@ -149,20 +138,13 @@ public class ClientSocket extends Thread implements ClientInterface {
 
     }
 
-
     @Override
-    public void createUseWeaponCard(String nicknamePlayer) throws RemoteException {
+    public void createUseWeaponCardMessage(String nicknamePlayer, int[] weaponCard, int[] featuresAvailable, int[][] playersToAttack, String error) throws RemoteException {
 
     }
 
     @Override
-    public void createPowerUpChoice(String nicknamePlayer, String[] descriptionPowerUp, int[] idPowerUp) throws RemoteException {
-
-    }
-
-
-    @Override
-    public void createUsePowerUpCard(String nicknamePlayer) throws RemoteException {
+    public void createUsePowerUpCard(String nicknamePlayer, int[] featuresAvailable, boolean[] stateCard, int stateGame, boolean attacked, int[] effectCard, String error) throws RemoteException {
 
     }
 
@@ -174,6 +156,11 @@ public class ClientSocket extends Thread implements ClientInterface {
 
     @Override
     public void createFailRegistration(String nicknamePlayer) throws RemoteException {
+
+    }
+
+    @Override
+    public void createChoiceCard(String nicknamePlayer, String[] descriptionPowerUp, int[] idPowerUp, String error, boolean discardOne, boolean isPowerUp) throws RemoteException {
 
     }
 
